d0bb719d2e53c06bd82cc285109b12811bd804f0
Pruthvi Prodduturi <14803523+PruthviProdduturi@users.noreply.github.com>
2026-09-24 20:03:49 -0700

    retirement: make replay startup observable

--- api/run_query_history_replay.py
+++ api/run_query_history_replay.py
@@ -1,10 +1,12 @@
 """Operator entrypoint for the bounded query-history retirement replay."""
 
-from run_system_authority_replay import main
 import sys
 
 sys.stdout.reconfigure(line_buffering=True)
 sys.stderr.reconfigure(line_buffering=True)
+print("query_history replay entrypoint starting", flush=True)
+
+from run_system_authority_replay import main
 
 
 if __name__ == "__main__":
